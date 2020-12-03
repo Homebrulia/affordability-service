6a58591151882f1cf8926e6e1e42ebebeb00a84f
TimothyAkana <TimothyAkana@gmail.com>
2020-12-03 10:36:44 -0800

    Seeding script for id +  home name + home price

--- sdcSeeding/dataGeneration.js
+++ sdcSeeding/dataGeneration.js
@@ -1,14 +1,53 @@
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
 
-
-const generateDataRecords = (numRecords) => {
-  const prices = [];
-  while (index < numRecords) {
-    let index = 0;
-    let randomPrice = 10000 * (Math.floor(Math.random() * 250) + 50)
-    prices.push({mortgageId: index, price: randomPrice})
-    index++;
+const lorem = new LoremIpsum({
+  wordsPerSentence: {
+    max: 4,
+    min: 1
   }
-  return prices;
+})
+
+console.log(lorem.generateSentences(1));
+/*  NEEDED FIELDS
+HOME PRICES:
+id
+name
+price
+
+DISCOUNTS:
+id
+name
+price
+start date
+end date
+max discount points
+min down payment
+min interest rate
+low income
+veteran
+combinable
+foreign key to a home price
+*/
+
+//Eventually aiming for 10 Million homes (primary records)
+const generateHomePrices = (numHomes) => {
+  let homePriceData = [];
+  let id = 1;
+  while (id <= numHomes) {
+    let name = lorem.generateSentences(1);
+    let price = 10000 * (Math.floor(Math.random() * 250) + 50);
+    homePriceData.push({id: id, name: name, price: price})
+    id++;
+  }
+  return homePriceData;
 }
 
-console.log(generateDataRecords(10000000));
+
+//Eventually aiming for a rough 1 Million discounts (roughly 10% of homes have discounts)
+const generateDiscounts = (numDiscounts) => {
+
+}
+
+const generateDataRecords = (numRecords) => {
+
+}
