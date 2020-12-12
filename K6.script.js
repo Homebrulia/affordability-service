226bac571a58874b8deff9e22cd0a52a83c5a891
TimothyAkana <TimothyAkana@gmail.com>
2020-12-12 15:51:34 -0800

    Modified K6 script to allow over 1k requests per second

--- K6.script.js
+++ K6.script.js
@@ -2,14 +2,20 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  vus: 1,
-  duration: '10s',
+  stages: [
+    {duration: "2m", target: 1},
+    {duration: "2m", target: 10},
+    {duration: "2m", target: 100},
+    {duration: "2m", target: 1000},
+    {duration: "2m", target: 2000},
+    {duration: "30s", target: 2000}
+  ],
 };
 
 export default function () {
   const home_id = Math.floor(Math.random() * 10000000)
-  http.get(`http://localhost:8020/mortgage/${home_id}`, {
+  http.get(`http://localhost:8020/mortgage/${home_id}/db`, {
     tags: {name: 'getHomeData'},
   });
-  sleep(0.6);
+  sleep(0.2);
 }
